11513a9d9976d77d5d6f6e79bde55cbfc2bbc8fa
Vitor <vitordiasalves@gmail.com>
2025-06-28 13:50:32 -0300

    Tagged template literal para definir jogador vencedor - Cria uma tagged function recebendo objetos com nome e pontos - Determina qual o jogador com mais pontos - Deixa nome dos jogadores que perderam em vermelho - Deixa nome do jogador que venceu verde - Usa chalk e strip-ansi

--- template-literals/tag.js
+++ template-literals/tag.js
@@ -1,21 +1,32 @@
 import chalk from "chalk";
+import stripAnsi from "strip-ansi";
 
 function main(){
     // console.log( myTag`A Loba se chama Ana` );
-    const jogadores = [{"nome":"Ana", "pontos":100}, {"nome":"Vitor", "pontos":90}];
-    console.log(definirVencerdor`Os jogadores são ${ jogadores[0] } e ${ jogadores[1] }`);
+    const Listajogadores = [{"nome":"Ana", "pontos":100}, {"nome":"Vitor", "pontos":90}, {"nome":"Ashley", "pontos":60}];
+    console.log(definirVencerdor`Os jogadores são: ${ Listajogadores[2] }, ${ Listajogadores[0] } e ${ Listajogadores[1] }`);
 }
+
+function definirVencerdor(string, ...jogadores){
+    let indexJogadorVencedor = 0;
+    jogadores.forEach((jogador, index) => {
+        if(jogador.pontos > jogadores[indexJogadorVencedor].pontos){indexJogadorVencedor = index} 
+         jogador.nome = chalk.red(jogador.nome);
+    });
+
+    jogadores[indexJogadorVencedor].nome = chalk.green(stripAnsi(jogadores[indexJogadorVencedor].nome));
+
+    console.log(string.reduce( (textoCompleto, palavra, index) => {
+        const nomeJogador = jogadores[index] !== undefined ? jogadores[index].nome : "";
+        return textoCompleto + palavra + nomeJogador;
+    }, ''));
+
+}
+
+main()
 
 function myTag(string){
     string[0].trim()
     .split(/\s+/)
     .forEach((word, index) => console.log(`${index} - ${word}`));
 }
-
-function definirVencerdor(string, jogador1, jogador2){
-    console.log(jogador1.pontos > jogador2.pontos ? chalk.green(jogador1.nome) : chalk.green(jogador2.nome) );
-      
-
-}
-
-main()
