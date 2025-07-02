764c5aa39d4cb4fbfd554c407b2de4d9fc4166ae
Vitor Dias <vitordiasalves@gmail.com>
2025-07-02 10:15:46 -0300

    Import e export     - shire.js exporta quatro variáveis diferentes     - rivendell.js importa todas as variáveis exportadaas por shire.js, transforma esse onbjeto em erray e adiciona cada elemento no array comitiva

---
+++ import_export/moria.js
@@ -0,0 +1,4 @@
+import comitiva from "./rivendell.js";
+
+const gandalf = comitiva.find( (membro) => membro.nome.includes("Gandalf") );
+console.log(`A comitiva entre em Moria quando ${gandalf.nome} fala 'AMIGO'`);
