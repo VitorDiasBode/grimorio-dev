81438efbd8709e79f97966de24130a7e0526b907
Vitor <vitordiasalves@gmail.com>
2025-09-04 15:53:58 -0300

    update: implementa do while e função com retorno

--- CS50/cat/cat.c
+++ CS50/cat/cat.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
- void meow(int n);
+void meow(int n);
+int get_positive_int(void);
 
 int main(void)
 {
-    meow(40);
+    int times = get_positive_int();
+    meow(times);
+}
+
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        printf("Number: ");
+        scanf("%d", &n);
+    } while (n < 1);
+
+    return n;
 }
 
 void meow(int n)
@@ -14,6 +28,25 @@ void meow(int n)
         printf("meow\n");
     }
 }
+
+/////////////////////
+
+// #include <stdio.h>
+
+//  void meow(int n);
+
+// int main(void)
+// {
+//     meow(40);
+// }
+
+// void meow(int n)
+// {
+//     for ( int i = 0; i < n; i++)
+//     {
+//         printf("meow\n");
+//     }
+// }
 
 /////////////////////
 
