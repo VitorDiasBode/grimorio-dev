3ab61e9c8e6c86516ead2f9ba090549f92de17de
Vitor <vitordiasalves@gmail.com>
2025-09-04 16:36:10 -0300

    feat: implmenta limite em casa decimais para numero float

--- CS50/calculator/calculator.c
+++ CS50/calculator/calculator.c
@@ -3,21 +3,39 @@
 
 int main(void)
 {
-    long dollars = 1;
+    int x;
+    int y;
 
-    while(true)
-    {
-        printf("Here's $%li. Double it and give it to the next person? ", dollars);
-        
-        char answer;
-        scanf(" %c", &answer);
-        
-        if (answer == 'y')
-        {
-            dollars *= 2;
-        }else{
-            break;
-        }
-    }
-    printf("Here's $%li.\n", dollars);
+    printf("x: ");
+    scanf("%i", &x);
+    printf("y: ");
+    scanf("%i", &y);
+
+    printf("result: %.5f\n", (float) x/y);
 }
+
+//////////////////////
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main(void)
+// {
+//     long dollars = 1;
+
+//     while(true)
+//     {
+//         printf("Here's $%li. Double it and give it to the next person? ", dollars);
+        
+//         char answer;
+//         scanf(" %c", &answer);
+        
+//         if (answer == 'y')
+//         {
+//             dollars *= 2;
+//         }else{
+//             break;
+//         }
+//     }
+//     printf("Here's $%li.\n", dollars);
+// }
