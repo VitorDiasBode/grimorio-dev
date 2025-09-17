9435532f217e7ffa60cff35116799c307376b1a2
Vitor <vitordiasalves@gmail.com>
2025-09-17 10:57:07 -0300

    chore: inicia a estrutura inicial das rotas

---
+++ react-router/nav-oceano-da-conquista/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <main>
+            <h1>Página Principal</h1>
+            <p>Bem vindo! Aqui usamos <b>Link</b> para criar navegação dentro do conteúdo.</p>
+        </main>
+    )
+}
+
+export default Home
