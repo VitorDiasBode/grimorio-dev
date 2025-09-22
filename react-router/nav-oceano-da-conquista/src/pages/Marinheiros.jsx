fe74f0a028d9e18809020b076084e23e831383c2
Vitor <vitordiasalves@gmail.com>
2025-09-22 12:14:30 -0300

    update: cada página mostra os personagem daquela categoria

--- react-router/nav-oceano-da-conquista/src/pages/Marinheiros.jsx
+++ react-router/nav-oceano-da-conquista/src/pages/Marinheiros.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import { personagens } from "../data";
+import CartaoDoPersonagem from "../components/CartaoDoPersonagem/CartaoDoPersonagem";
 
-function Marinheiros() {
+export default function Marinheiros() {
+    const marinheiros = personagens.filter( (personagem) => personagem.categoria == 'marinheiros');
+    
     return (
         <div>
             <Navbar />
-            Marinheiros
+
+            <h1>Marinheiros</h1>
+
+            <ul>
+                {marinheiros.map( (marinheiro) => (
+                    <li key={marinheiro.key}>
+                        <CartaoDoPersonagem nome={marinheiro.nome} categoria={marinheiro.categoria}/>
+                    </li>    
+                ))}
+            </ul>
         </div>
     )
 }
 
-export default Marinheiros
--- react-router/nav-oceano-da-conquista/src/pages/Piratas.jsx
+++ react-router/nav-oceano-da-conquista/src/pages/Piratas.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import { personagens } from "../data";
+import CartaoDoPersonagem from "../components/CartaoDoPersonagem/CartaoDoPersonagem";
 
 function Piratas() {
+    const piratas = personagens.filter( (personagem) => personagem.categoria == 'piratas');
+
     return(
         <div>
             <Navbar />
-            Piratas
+
+            <h1>Piratas</h1>
+
+            <ul>
+                {piratas.map( (pirata) => (
+                    <li key={pirata.key}>
+                        <CartaoDoPersonagem nome={pirata.nome} categoria={pirata.categoria}/>
+                    </li>    
+                ))}
+            </ul>
         </div>
     )
 }
