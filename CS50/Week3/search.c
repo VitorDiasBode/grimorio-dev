6d51c95ba5c6294e6649de0680ba8834831a6b1f
Vitor <vitordiasalves@gmail.com>
2025-09-25 16:56:35 -0300

    update: atualiza o algoritmo de linear search para buscar strings

--- CS50/Week3/search.c
+++ CS50/Week3/search.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
+    char *strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+    
+    char s[50];
+    printf("String: ");
+    scanf("%s", &s);
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (strcmp(strings[i], s ) == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
 
 }
 
