dea1a24cf7a0b7c182ec052b3d2c1871948eb568
Vitor <vitordiasalves@gmail.com>
2025-09-25 16:42:03 -0300

    feat: implementa algoritmo de linear search com números inteiros

---
+++ CS50/Week3/search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+}
+
+/////////////////////////////////////
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+//     int n;
+//     printf("Number: ");
+//     scanf("%i", &n);
+
+//     for (int i = 0; i < 7; i++)
+//     {
+//         if (numbers[i] == n)
+//         {
+//             printf("Found\n");
+//             return 0;
+//         }
+//     }
+//     printf("Not found\n");
+//     return 1;
+
+// }
