e85f737be89b19e032a843b2ca9c2c9d8ba407e1
Vitor Dias <vitordiasalves@gmail.com>
2025-10-07 16:13:47 -0300

    feat: implementa uma linked list com nodes que armazenam números

---
+++ CS50/Week5/linked_list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}node;
+
+int main(void)
+{
+    node *list = NULL;
+
+    for (int i=0; i < 3; i++)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
+
+        printf("Number: ");
+        scanf("%i", &n->number);
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
+    }
+
+    //Time Passes
+
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("VALOR :%i\n", ptr->number);
+        node *last_n = ptr;
+        ptr = ptr->next;
+        free(last_n);
+    }
+    
+    return 0;
+}
