10c24759a734dad36b70f37bd5f8a2c98bacc966
Vitor <vitordiasalves@gmail.com>
2025-11-10 18:05:44 -0300

    update: implementa o envio de dados via POST usando o Jinja
    - um array methods com os metodos aceitos na rota / é passado
    - nessa rota, quando o método post for usado, a pagina greet é renderizada e o dado name é passado como parâmetro
    - na pagina greet, usando as propriedades do Jinja, é checado para determinar qual valor vai ser mostrado

--- CS50/Week9/app.py
+++ CS50/Week9/app.py
@@ -3,12 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():    
-    return render_template("index.html")
-
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.form.get("name", "world")
-    
-    return render_template("greet.html", name=name)
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return render_template("greet.html", name=request.form.get("name"))
