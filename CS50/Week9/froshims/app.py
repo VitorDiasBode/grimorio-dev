8fd43990d3a6c9232c33a2c189f27696782fe412
Vitor <vitordiasalves@gmail.com>
2025-11-13 11:07:27 -0300

    update: valida se os dados do formulário não são vazios

--- CS50/Week9/froshims/app.py
+++ CS50/Week9/froshims/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "deu certo"
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport"):
+        return render_template("failure.html")
+    return render_template("success.html")
